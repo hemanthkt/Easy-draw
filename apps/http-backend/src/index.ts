c7bcd9551badf0b8eed42a5030d58cdc3c4130f8
hemanthkt <hmkt1503@gmail.com>
2025-05-18 23:13:52 +0530

    CLI initialised

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express"
+
+const app = express()
+
+
+
+app.get("/signup", (req,res) => {
+    
+})
+
+app.get("/signup", (req,res) => {
+    
+})
+
+app.get("/createroom", (req,res) => {
+    
+})
+
+app.listen(3001)

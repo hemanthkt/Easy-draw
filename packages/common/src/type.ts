133a2d20d2b182555a1197c468452bb39a08c38f
hemanthkt <hmkt1503@gmail.com>
2025-05-21 15:20:57 +0530

    added prisma

---
+++ packages/common/src/type.ts
@@ -0,0 +1,20 @@
+import {z} from "zod"
+
+
+export const CreateUserSchema = z.object({ 
+    username: z.string().min(5, "Username must contain atleast 2 charecters").max(20),
+    password: z.string().min(8, "Password shouldn't be less than 8 charecters").max(20),
+    name: z.string()
+})
+
+
+export const SigninSchema = z.object({
+    username: z.string().min(5, "Username must contain atleast 2 charecters").max(20),
+    password: z.string().min(8, "Password shouldn't be less than 8 charecters").max(20),
+ })
+
+ export const CreateRoomSchema = z.object({
+    name: z.string().min(3).max(20),
+     
+ })
+

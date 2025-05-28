fa624c190ee3f3809c8a10c5ca051f89361f9bb3
hemanthkt <hmkt1503@gmail.com>
2025-05-28 23:15:45 +0530

    pushes existing shapes

--- apps/drawapp-fe/draw/index.ts
+++ apps/drawapp-fe/draw/index.ts
@@ -1,7 +1,21 @@
+type Shapes = {
+  type: "rect"
+  x: number
+  y: number
+  width: number
+  height: number
+} | {
+  type: "circle"
+  centerX : number
+  centerY : number
+  radius: number
+
+}
 export default function initDraw(canvas: HTMLCanvasElement) {
+  
 
        const ctx = canvas.getContext("2d");
-
+      let existingShape: Shapes[] = []
       if (!ctx) {
         return;
       }
@@ -20,20 +34,43 @@ export default function initDraw(canvas: HTMLCanvasElement) {
       });
       canvas.addEventListener("mouseup", (e) => {
         clicked = false;
+        const width  = e.clientX - startX
+        const height = e.clientY - startY
+
+        existingShape.push({
+          type: "rect",
+          x: startX,
+          y: startY,
+          width,
+          height 
+        })
       });
 
       canvas.addEventListener("mousemove", (e) => {
         if (clicked) {
           let width = e.clientX - startX;
           let height = e.clientY - startY;
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.fillStyle = "rgba(0,0,0)";
-          ctx.fillRect(0, 0, canvas.width, canvas.height);
+          clearCanvas(existingShape, canvas, ctx)                           
+         
           ctx.strokeStyle = "rgba(255,255,255)";
           ctx.strokeRect(startX, startY, width, height);
-
           console.log(e.clientX);
           console.log(e.clientY);
         }
       });       
+}
+
+function clearCanvas (existingShape: Shapes[], canvas: HTMLCanvasElement, ctx:CanvasRenderingContext2D){
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          ctx.fillStyle = "rgba(0,0,0)";
+          ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+          existingShape.map((shape) => {
+            if(shape.type == "rect"){
+               ctx.strokeStyle = "rgba(255,255,255)";
+          ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+            }
+          })
+          
+
 }
